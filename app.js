fcff5d96f0f50c42dd055ecce29af5a067d8461a
Samuelichu <chavezzsam@gmail.com>
2025-08-08 18:12:00 -0500

    Creando funcion para actualizar lista de Amigos

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ let arrayAmigos = [];
 
 const agregarAmigo = () => {
   let textAmigo = document.getElementById("amigo").value;
-  arrayAmigos.push(textAmigo);
-  console.log(arrayAmigos)
+  if (textAmigo === "") {
+    alert("Por favor inserte un nombre");
+  } else {
+    arrayAmigos.push(textAmigo);
+    listaAmigo();
+    console.log(arrayAmigos);
+  }
+};
+
+const listaAmigo = () => {
+  const listAmigo = document.getElementById("listamigos");
+  listAmigo.innerHTML = "";
+  arrayAmigos.forEach((a) => {
+    const lista = document.createElement("li");
+    lista.innerHTML = a;
+    listAmigo.appendChild(lista);
+  });
 };
